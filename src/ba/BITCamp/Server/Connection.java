985fb8189febba5787fc7c7d7a5dc08cd9ee5e2e
EminaMuratovic <emina_peace@hotmail.com>
2015-02-20 16:28:06 +0100

    ...

--- src/ba/BITCamp/Server/Connection.java
+++ src/ba/BITCamp/Server/Connection.java
@@ -35,48 +35,53 @@ public class Connection implements Runnable {
 			return;
 		}
 		String str = null;
+		String tempLine = null;
 		try {
-			while ((str = read.readLine()) != null) {
-				if (str.contains("GET") || str.isEmpty()) {
-					break;
+			while ((tempLine = read.readLine()) != null) {
+				if (tempLine.contains("GET") || tempLine.contains("POST")) {
+					str = tempLine;
+					if (str.contains("GET"))
+						break;
 				}
+				System.out.println(tempLine);
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-			if (!str.contains("GET")) {
-				Logger.log("warning", "Was not GET request");
-				Response.error(write, "Invalid request");
-				closeClient();
-				return;
-			}
-			String fileName = getFileName(str);
-			FileInputStream fis = null;
-			try {
-				fis = new FileInputStream(fileName);
-			} catch (FileNotFoundException e) {
-				Response.error(write, "This is not the page you are looking for");
-				Logger.log("warning", "File not found" + e.getMessage());
-				closeClient();
-				return;
-			}
-			BufferedReader br = new BufferedReader(new InputStreamReader(fis));
-			String fileLine = "";
-			StringBuilder sb = new StringBuilder();
-			
-			try {
-				while ((fileLine = br.readLine()) != null)
-					sb.append(fileLine);
-			} catch (IOException e) {
-				Response.serverError(write, "A well trained group of monkeys is trying to fix the problem.");
-				Logger.log("error", e.getMessage());
-				closeClient();
-				return;
-			}
-			Response.ok(write,sb.toString());
+		if (!str.contains("GET")) {
+			Logger.log("warning", "Was not GET request");
+			Response.error(write, "Invalid request");
 			closeClient();
+			return;
+		}
+		String fileName = getFileName(str);
+		FileInputStream fis = null;
+		try {
+			fis = new FileInputStream(fileName);
+		} catch (FileNotFoundException e) {
+			Response.error(write, "This is not the page you are looking for");
+			Logger.log("warning", "File not found" + e.getMessage());
+			closeClient();
+			return;
+		}
+		BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+		String fileLine = "";
+		StringBuilder sb = new StringBuilder();
+
+		try {
+			while ((fileLine = br.readLine()) != null)
+				sb.append(fileLine);
+		} catch (IOException e) {
+			Response.serverError(write,
+					"A well trained group of monkeys is trying to fix the problem.");
+			Logger.log("error", e.getMessage());
+			closeClient();
+			return;
+		}
+		Response.ok(write, sb.toString());
+		closeClient();
 
 	}
 
@@ -89,16 +94,15 @@ public class Connection implements Runnable {
 				break;
 			}
 		String basePath = "." + File.separator + "html" + File.separator;
-		if(fileName == null || fileName.equals("/"))
+		if (fileName == null || fileName.equals("/"))
 			return basePath + "index.html";
-		
-		if(!fileName.contains("."))
+
+		if (!fileName.contains("."))
 			fileName += ".html";
 		return basePath + fileName;
-		
 
 	}
-	
+
 	private void closeClient() {
 		try {
 			client.close();
